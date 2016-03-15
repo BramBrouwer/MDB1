6d5961d6a73a953b0b966b372d9d1ff9cc6fef52
Alec <alec@Alec-Laptop>
2016-03-15 17:34:48 +0100

    api update

--- www/js/API.js
+++ www/js/API.js
@@ -3,13 +3,12 @@ $( document ).ready(function() {
     this.typeURL;
     this.APIKey = '?api_key=15dfe91d-08ef-4fc9-a622-8d76c79c1ec5';
 
-    function getSummonerData(sumName, region){
+    this.getSummonerData = function(sumName, region){
         this.typeURL = '/api/lol/' + region + '/v1.4/summoner/by-name/' + sumName + this.APIKey;
-        this.xhr = new XMLHttpRequest();
-        this.xhr.open("GET", this.baseURL + this.typeURL, false);
-        this.xhr.send(null);
-        this.raw =  JSON.parse(this.xhr.responseText);
-        this.data = this.raw[sumName.toLowerCase()];
-        $('#name').append('<p>' + this.data['name'] + '</p>');
+        $.get(this.baseURL + this.typeURL, function(response) {
+            if(this.sumData = response[sumName.toLowerCase()]){
+                $('#name').append('<p>' + this.sumData['name'] + '</p>');
+            }
+        });
     }
 });
