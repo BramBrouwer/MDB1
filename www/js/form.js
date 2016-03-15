66d53640066fe38b9e3cb2ae08ea0fe318599710
bbrouwer <b.brouwer@student.avans.nl>
2016-03-15 16:39:47 +0100

    Memes

--- www/js/form.js
+++ www/js/form.js
@@ -9,6 +9,16 @@ $('#target').submit(function() {
   console.log("Summoner name 2 = " + $('input[name="s2"]').val());
   
   
+    // $.getJSON("https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/Henksteentjes?api_key=6cbf8df1-8340-4de9-bf1d-f1034b3b7171", function(result){
+    //     $.each(result, function(i, field){
+    //         // $("div").append(field + " ");
+    //         console.log(field);
+    //     });
+    // });
+    
+    
+ 
+  
   //TODO: valideer namen
 //   Return false zodat de action niet aangeroepen word, dit wordt straks een verwijzing naar de volgende stap van het submitten
   return false;
