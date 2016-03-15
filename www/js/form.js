22946646d1f938aa8a13edee0aa5ec324b174822
bbrouwer <b.brouwer@student.avans.nl>
2016-03-15 16:11:16 +0100

    Onzin

--- www/js/form.js
+++ www/js/form.js
@@ -8,6 +8,13 @@ $('#target').submit(function() {
   console.log("Summoner name 1 = " + $('input[name="s1"]').val());
   console.log("Summoner name 2 = " + $('input[name="s2"]').val());
   
+  
+  //TODO: valideer namen
 //   Return false zodat de action niet aangeroepen word, dit wordt straks een verwijzing naar de volgende stap van het submitten
   return false;
+  
+  
+  
+  //namen gevalideerd? Voer submit actie uit
+  
 });
