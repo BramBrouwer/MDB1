7712b3beba30acbf8bcb94801d0fe6c5fc25a177
bbrouwer <b.brouwer@student.avans.nl>
2016-03-18 02:04:59 +0100

    Async, loader, error message
    Asynchronous ajax call, loader added while waiting on ajax call, error
    messages added

--- www/js/form.js
+++ www/js/form.js
@@ -1,42 +1,108 @@
-// CSS loader toevoegen en ajax call weer syncrhonous maken zodat er een laadscherm staat terwijl de summonerinfo opgehaald word
-// er dus voor zorgen dat ajax call wordt uitgevoerd bij submit en pas bij succes callback verdergaat 
-function getSummonerData (region, summonerName){
- var baseURL = 'https://euw.api.pvp.net';
- var typeURL;
- var APIKey = '?api_key=15dfe91d-08ef-4fc9-a622-8d76c79c1ec5';
- var typeURL = '/api/lol/' + region + '/v1.4/summoner/by-name/' + summonerName + APIKey;
- var result ="";
+//Ajax start and stop listeners to show/hide loader
+$( document ).ajaxStart(function() {
+ $('#loader').show();
+});
 
-     $.ajax({
-         url: baseURL+typeURL, 
-         async: false,
-         success: function(data){
-console.log(data);
-                result = data;
-               
-    }});
-    return result;
+$( document ).ajaxStop(function() {
+ $('#loader').hide();
+
+});
+
+
+// get name example
+//  console.log(summoner2[name_2]);
+
+$('#target').submit(function() {
+   
+ 
+ 
+ 
+ //Function vairables
+ var baseURL = 'https://euw.api.pvp.net';
+ var APIKey = '?api_key=15dfe91d-08ef-4fc9-a622-8d76c79c1ec5';
+ var region ='euw';
+ var summoner1;
+ var summoner2;
+ 
+  //retrieve names
+  var name_1 = $('input[name="s1"]').val().toLowerCase();
+  var name_2 = $('input[name="s2"]').val().toLowerCase();
+  var typeURL1 = '/api/lol/' + region + '/v1.4/summoner/by-name/' + name_1 + APIKey;
+  var typeURL2 = '/api/lol/' + region + '/v1.4/summoner/by-name/' + name_2 + APIKey;
+ 
+   if(name_1 == "" || name_2 == "")
+   {
+       empty_name_error();
+       return false;
+   }
+
+       //first ajax call, verify the first summoner name given
+         $.ajax({
+            url: baseURL+typeURL1, 
+               success: function(data1)
+                {
+                //First name verified
+                console.log('First summoner name verified');
+                summoner1 = data1;
+                console.log(summoner1);
+                
+                //second ajax call, verify second name
+                  $.ajax({
+                  url: baseURL+typeURL2, 
+                  success: function(data2)
+                   {
+                    //Second name verified
+                    console.log('Second summoner name verified');
+                    summoner2 = data2;
+                    console.log(summoner2);
+                    // Both names verified
+                    //TODO pass names to next step, confirm name validation
+                    // Both names verified
+                  },
+                  //Second name 404
+                   error : function(error) 
+                    {
+                    console.log(error);
+                    errorpopup(name_2);
+                    }
+                  
+                  
+                  });
+         
+                },
+                //First name 404    
+                error : function(error) 
+                {
+                console.log(error);
+                errorpopup(name_1);
+
+                }
+              
+          });
+          return false;
+
+})
+
+
+function errorpopup(invalidName)
+{
+    //first empty the containers text
+    $("#popupcontent").empty();
+    //then we add the error message corresponding with the relevant name
+    $("#popupcontent").append('Invalid username: '+invalidName)
+
+    //open popup
+     $( "#name_error" ).popup( "open");
+}
+
+function empty_name_error(){
+     //first empty the containers text
+    $("#popupcontent").empty();
+    $("#popupcontent").append('Please enter both fields');
+    $( "#name_error" ).popup( "open");
 }
 
 
 
 
 
-
-$('#target').submit(function() {
-  
-   //retrieve names
-  var name_1 = $('input[name="s1"]').val().toLowerCase();
-  var name_2 = $('input[name="s2"]').val().toLowerCase();
-
-//  Retrieve summoners from lol api
-  var summoner1 = getSummonerData('EUW',name_1);
-  var summoner2 = getSummonerData('EUW',name_2);
-
-    console.log(summoner1[name_1].name)
- console.log(summoner2[name_2].name)
-  
-  return false;
-  
-  
-});
