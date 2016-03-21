c95fc25d4a09d50a6882bcc4990d9c5f04f2ecf6
bbrouwer <b.brouwer@student.avans.nl>
2016-03-21 11:42:32 +0100

    getstasnormals toegevoegd

--- www/js/form.js
+++ www/js/form.js
@@ -1,22 +1,22 @@
 
-
+//Function vairables
+ var baseURL = 'https://euw.api.pvp.net';
+ var APIKey = 'api_key=15dfe91d-08ef-4fc9-a622-8d76c79c1ec5';
+ var region ='euw';
 // get name example
 //  console.log(summoner2[name_2]);
 
 $('#target').submit(function() {
  
- //Function vairables
- var baseURL = 'https://euw.api.pvp.net';
- var APIKey = '?api_key=15dfe91d-08ef-4fc9-a622-8d76c79c1ec5';
- var region ='euw';
+ 
  var summoner1;
  var summoner2;
  
   //retrieve names
   var name_1 = $('input[name="s1"]').val().toLowerCase();
   var name_2 = $('input[name="s2"]').val().toLowerCase();
-  var typeURL1 = '/api/lol/' + region + '/v1.4/summoner/by-name/' + name_1 + APIKey;
-  var typeURL2 = '/api/lol/' + region + '/v1.4/summoner/by-name/' + name_2 + APIKey;
+  var typeURL1 = '/api/lol/' + region + '/v1.4/summoner/by-name/' + name_1 +'?' +APIKey;
+  var typeURL2 = '/api/lol/' + region + '/v1.4/summoner/by-name/' + name_2 +'?' +APIKey;
  
    if(name_1 == "" || name_2 == "")
    {
@@ -47,8 +47,9 @@ $('#target').submit(function() {
                     
                     // Both names verified
                   $('#loader').hide();
+                
                   $( ":mobile-pagecontainer" ).pagecontainer( "change", "compare.html", { role: "page" } );
-                   getstatsnormal(summoner1,summoner2);
+                   getstatsnormal(summoner1,summoner2,name_1,name_2);
 
                     
                   },
@@ -88,15 +89,67 @@ $('#target').submit(function() {
 
 
 // End get stats
-function getstatsnormal(sum1,sum2){
-// TODO retrieve both summer stats and log to console
+function getstatsnormal(sum1,sum2,name1,name2){
+ //declare variables
+ var summoner1stats;
+ var summoner2stats;
+
+//  console.log(sum2[name2]);
+  var typeURL1 = '/api/lol/' + region + '/v1.3/stats/by-summoner/' + sum1[name1].id +'/summary?season=SEASON2016&'+APIKey;
+  var typeURL2 = '/api/lol/' + region + '/v1.3/stats/by-summoner/' + sum2[name2].id +'/summary?season=SEASON2016&'+APIKey;
+ 
+                  $.ajax({
+                  url: baseURL+typeURL1, 
+                  success: function(data1)
+                   {
+                    
+                    summoner1stats = data1;
+                    console.log(summoner1stats);
+                    
+                      $.ajax({
+                  url: baseURL+typeURL2, 
+                  success: function(data2)
+                   {
+                    
+                    summoner2stats = data2;
+                    // console.log(summoner2stats.playerStatSummaries);
+                    calculateStats(summoner2stats.playerStatSummaries);
+                    //beide summoners opgehaald
+                    
+                    
+                  },
+                 
+                   error : function(error) 
+                    {
+                    console.log(error);
+                    
+                    
+                    }
+
+                  });
+                    
+                    
+                  },
+                 
+                   error : function(error) 
+                    {
+                    console.log(error);
+                    
+                    
+                    }
+
+                  });
+
 
 }
 
 
 
 
-
+// CalculateStats
+function calculateStats(summonerstats){
+    console.log(summonerstats);
+}
 
 
 
@@ -123,6 +176,15 @@ function empty_name_error(){
 
 function succes_message(){
   
+//   CAP 5x5 + unranked + 3v3 undraked = totale normal stats
+// 			-Creep score 
+// 			-championkills
+// 			-assists
+// 			-turrets killed
+// 			-wins
+
+
+
     $( "#success_popup" ).popup( "open");
 }
 // End Popups
