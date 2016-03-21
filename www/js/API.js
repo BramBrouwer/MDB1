36560ddfd4e9f6af43d3fad922ede96465b839e7
Alec <alec@Alec-Laptop>
2016-03-21 08:52:27 +0100

    kek

--- www/js/API.js
+++ www/js/API.js
@@ -1,16 +1,19 @@
 window.onload = function(){
-    this.baseURL = 'https://euw.api.pvp.net';
-    this.typeURL;
-    this.APIKey = '?api_key=15dfe91d-08ef-4fc9-a622-8d76c79c1ec5';
-
-    function getSummonerData(sumName, region){
-        this.typeURL = '/api/lol/' + region + '/v1.4/summoner/by-name/' + sumName + this.APIKey;
-        $.get(this.baseURL + this.typeURL, function(response) {
-            if(this.sumData = response[sumName.toLowerCase()]){
-                $('#name').append('<p>' + this.sumData['name'] + '</p>');
-            }
-        });
-    }
     
-    $('#getNameBtn').on('click', function(){getSummonerData('SalamiBami', 'euw');});
+
+
+    function connection(){
+        this.baseURL = 'https://euw.api.pvp.net';
+        this.typeURL;
+        this.APIKey = '?api_key=15dfe91d-08ef-4fc9-a622-8d76c79c1ec5';
+        
+        this.getSummonerData = function(sumName, region){
+            this.typeURL = '/api/lol/' + region + '/v1.4/summoner/by-name/' + sumName + this.APIKey;
+            $.get(this.baseURL + this.typeURL, function(response) {
+                if(this.sumData = response[sumName.toLowerCase()]){
+                    return this.sumData['name'];
+                }
+            });
+        }
+    }
 };
