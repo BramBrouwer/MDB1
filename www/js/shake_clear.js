b0d8df503722c13559edfd86b90b6a4d8df7c1c3
bbrouwer <b.brouwer@student.avans.nl>
2016-03-21 13:53:15 +0100

    Shake listener

---
+++ www/js/shake_clear.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    alert('test');
+    // Start watching for shake gestures and call "onShake"
+// with a shake sensitivity of 40 (optional, default 30)
+shake.startWatch(onShake, 40 ,onError);
+
+// Stop watching for shake gestures
+shake.stopWatch();
+ });
+
+var onShake = function () {
+  // Fired when a shake is detected
+ console.log('shake detected');
+ 
+};
+
+var onError = function () {
+  // Fired when there is an accelerometer error (optional)
+};
+
