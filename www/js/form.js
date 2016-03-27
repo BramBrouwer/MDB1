dc477b8a6656bfbae5c5c0335ccb99fe25272966
Alec <alec@Alec-Laptop>
2016-03-27 22:56:30 +0200

    test3

--- www/js/form.js
+++ www/js/form.js
@@ -12,7 +12,7 @@ var name2 = '';
 
 $('#target').submit(function() {
     StatusBar.show();
-    StatusBar.backgroundColorByName('red');
+    StatusBar.backgroundColorByName("red");
     $('.stats_table').hide();
     $('#loader').show();
     resetScore();
