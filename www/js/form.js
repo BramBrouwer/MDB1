0c2f8dbd04dd3746f4fb813651ed41fb139ca2e3
Alec <alec@Alec-Laptop>
2016-03-27 22:28:58 +0200

    Score & minor fixes

--- www/js/form.js
+++ www/js/form.js
@@ -3,123 +3,97 @@ $(document).ready(function() {
     
  });
 //Function vairables
- var baseURL = 'https://euw.api.pvp.net';
- var APIKey = 'api_key=15dfe91d-08ef-4fc9-a622-8d76c79c1ec5';
- var region ='euw';
-// get name example
-//  console.log(summoner2[name_2]);
+var baseURL = 'https://euw.api.pvp.net';
+var APIKey = 'api_key=15dfe91d-08ef-4fc9-a622-8d76c79c1ec5';
+var region ='euw';
+var typeURL = '';
+var name1 = '';
+var name2 = '';
 
 $('#target').submit(function() {
     $('.stats_table').hide();
-  //    Disable button to prevent the user breaking stuff
-      $('input[type="submit"]').prop('disabled', true);
+    $('#loader').show();
+    resetScore();
+    name1 = $('input[name="s1"]').val().toLowerCase();
+    name2 = $('input[name="s2"]').val().toLowerCase();
     
- var summoner1;
- var summoner2;
+    // Disable button to prevent the user breaking stuff
+    $('input[type="submit"]').prop('disabled', true);
  
-  //retrieve names
-  var name_1 = $('input[name="s1"]').val().toLowerCase();
-  var name_2 = $('input[name="s2"]').val().toLowerCase();
-  var typeURL1 = '/api/lol/' + region + '/v1.4/summoner/by-name/' + name_1 +'?' +APIKey;
-  var typeURL2 = '/api/lol/' + region + '/v1.4/summoner/by-name/' + name_2 +'?' +APIKey;
- 
-   if(name_1 == "" || name_2 == "")
-   {
-       empty_name_error();
+    //retrieve names
+    verifySummoners();
+    
+    if(name1 == "" || name2 == "")
+    {
+        empty_name_error();
         $('input[type="submit"]').prop('disabled', false);
-       return false;
-   }
-        
-       $('#loader').show();
-   
-       //first ajax call, verify the first summoner name given
-         $.ajax({
-            url: baseURL+typeURL1, 
-               success: function(data1)
-                {
-                //First name verified
-                console.log('First summoner name verified');
-                summoner1 = data1;
-              
-                
-                //second ajax call, verify second name
-                  $.ajax({
-                  url: baseURL+typeURL2, 
-                  success: function(data2)
-                   {
-                    //Second name verified
-                    console.log('Second summoner name verified');
-                    summoner2 = data2;
+        return false;
+    }
+    return false;
+});
+
+function verifySummoners(){
+    var sum1;
+    var sum2;
+    var typeURL1 = '/api/lol/' + region + '/v1.4/summoner/by-name/' + name1 +'?' +APIKey;
+    var typeURL2 = '/api/lol/' + region + '/v1.4/summoner/by-name/' + name2 +'?' +APIKey;
+
+   //first ajax call, verify the first summoner name given
+    $.ajax({
+        url: baseURL+typeURL1, 
+        success: function(data1)
+        {   //First name verified
+            sum1 = data1;
+            
+            $.ajax({    //second ajax call, verify second name
+                url: baseURL+typeURL2, 
+                success: function(data2)
+                {   //Second name verified
+                    sum2 = data2;
                     $('input[type="submit"]').prop('disabled', false);
-
-                    
-                    // Both names verified
-                  
-                  //$( ":mobile-pagecontainer" ).pagecontainer( "change", "compare.html", { role: "page" } );
-                   getstatsnormal(summoner1,summoner2,name_1,name_2);
-
-                    
-                  },
-                  //Second name 404
-                   error : function(error) 
-                    {
+                    //$( ":mobile-pagecontainer" ).pagecontainer( "change", "compare.html", { role: "page" } );
+                    console.log(sum1, sum2);
+                    getStatsNormal(sum1,sum2);
+                },
+                //Second name 404
+                error : function(error)
+                {
                     console.log(error);
                      $('#loader').hide();
-                    errorpopup(name_2);
-                    $('input[type="submit"]').prop('disabled', false);
-                    
-                    }
-
-                  });
-         
-                },
-                //First name 404    
-                error : function(error) 
-                {
-                console.log(error);
-                 $('#loader').hide();
-                errorpopup(name_1);
-                $('input[type="submit"]').prop('disabled', false);
-
+                    errorpopup(name2);
+                    $('input[type="submit"]').prop('disabled', false);    
                 }
-              
-          });
-          return false;
+            });
+        },
+        //First name 404    
+        error : function(error) 
+        {
+            console.log(error);
+            $('#loader').hide();
+            errorpopup(name1);
+            $('input[type="submit"]').prop('disabled', false);
+        }      
+    });
+}
 
-})
+function getStatsNormal(sum1,sum2){
+    //declare variables
+    var summoner1stats;
+    var summoner2stats;
 
-
-
-
-// Get stats
-
-
-
-
-
-// End get stats
-function getstatsnormal(sum1,sum2,name1,name2){
- //declare variables
- var summoner1stats;
- var summoner2stats;
-
-//  console.log(sum2[name2]);
-  var typeURL1 = '/api/lol/' + region + '/v1.3/stats/by-summoner/' + sum1[name1].id +'/summary?season=SEASON2016&'+APIKey;
-  var typeURL2 = '/api/lol/' + region + '/v1.3/stats/by-summoner/' + sum2[name2].id +'/summary?season=SEASON2016&'+APIKey;
+    var typeURL1 = '/api/lol/' + region + '/v1.3/stats/by-summoner/' + sum1[name1].id +'/summary?season=SEASON2016&'+APIKey;
+    var typeURL2 = '/api/lol/' + region + '/v1.3/stats/by-summoner/' + sum2[name2].id +'/summary?season=SEASON2016&'+APIKey;
     $.ajax({
         url: baseURL+typeURL1, 
         success: function(data1)
         {           
             summoner1stats = data1['playerStatSummaries'];
-            summoner1stats['name'] = name1;
             $.ajax({
                 url: baseURL+typeURL2, 
                 success: function(data2)
                 {    
                     summoner2stats = data2['playerStatSummaries'];
-                    summoner2stats['name'] = name2;
                     calculateStats(summoner1stats, summoner2stats);
-                    //beide summoners opgehaald
                 }, 
                 error : function(error) 
                 {
@@ -132,74 +106,13 @@ function getstatsnormal(sum1,sum2,name1,name2){
             console.log(error);        
         }
     });
-    
 }
 
-
-
-
 // CalculateStats
-function calculateStats(sum1stats, sum2stats){
-    var stats = {'sum1Name': sum1stats['name'], 'sum2Name': sum2stats['name'], 'Unranked': null }
-    
-/* Summoner 1 */
-    var sum1unrankedStats = { 'CS': 0, 'Champion_Kills': 0, 'Assists': 0, 'Turret_Kills': 0, 'Wins': 0 };
-    sum1stats.forEach(function(entry){
-        switch(entry['playerStatSummaryType']) { //Set stats
-            case 'CoopVsAI':
-                var coopvsai = entry['aggregatedStats'];
-                break;
-            case 'OdinUnranked':
-                break;
-            case 'RankedTeam3x3':
-                var ranked3 = entry['aggregatedStats'];
-                break;
-            case 'RankedTeam5x5':
-                var ranked5 = entry['aggregatedStats'];
-                break;
-            case 'Unranked3x3':
-                var unranked3 = entry['aggregatedStats'];
-                sum1unrankedStats['CS'] += unranked3['totalMinionKills'];
-                sum1unrankedStats['CS'] += unranked3['totalNeutralMinionsKilled'];
-                sum1unrankedStats['Champion_Kills'] += unranked3['totalChampionKills'];
-                sum1unrankedStats['Assists'] += unranked3['totalAssists'];
-                sum1unrankedStats['Turret_Kills'] += unranked3['totalTurretsKilled'];
-                sum1unrankedStats['Wins'] += entry['wins'];
-                break;
-            case 'CAP5x5':
-                var teambuilder = entry['aggregatedStats'];
-                sum1unrankedStats['CS'] += teambuilder['totalMinionKills'];
-                sum1unrankedStats['CS'] += teambuilder['totalNeutralMinionsKilled'];
-                sum1unrankedStats['Champion_Kills'] += teambuilder['totalChampionKills'];
-                sum1unrankedStats['Assists'] += teambuilder['totalAssists'];
-                sum1unrankedStats['Turret_Kills'] += teambuilder['totalTurretsKilled'];
-                sum1unrankedStats['Wins'] += entry['wins'];
-                break;
-            case 'AramUnranked5x5':
-                var aram = entry['aggregatedStats'];
-                break;
-            case 'Unranked':
-                var unranked = entry['aggregatedStats'];
-                sum1unrankedStats['CS'] += unranked['totalMinionKills'];
-                sum1unrankedStats['CS'] += unranked['totalNeutralMinionsKilled'];
-                sum1unrankedStats['Champion_Kills'] += unranked['totalChampionKills'];
-                sum1unrankedStats['Assists'] += unranked['totalAssists'];
-                sum1unrankedStats['Turret_Kills'] += unranked['totalTurretsKilled'];
-                sum1unrankedStats['Wins'] += entry['wins'];
-                break;
-            case 'RankedSolo5x5':
-                var rankedsolo = entry['aggregatedStats'];
-                break;
-            default:
-                
-                break;
-        }
-    });
-/* Summoner 1 END */
+function getSummonerStats(sumstats){
+    var summonerstats = { 'CS': 0, 'Champion_Kills': 0, 'Assists': 0, 'Turret_Kills': 0, 'Wins': 0 };
 
-/* Summoner 2 */
-    var sum2unrankedStats = { 'CS': 0, 'Champion_Kills': 0, 'Assists': 0, 'Turret_Kills': 0, 'Wins': 0 }; 
-        sum2stats.forEach(function(entry){
+    sumstats.forEach(function(entry){
         switch(entry['playerStatSummaryType']) { //Set stats
             case 'CoopVsAI':
                 var coopvsai = entry['aggregatedStats'];
@@ -214,68 +127,74 @@ function calculateStats(sum1stats, sum2stats){
                 break;
             case 'Unranked3x3':
                 var unranked3 = entry['aggregatedStats'];
-                sum2unrankedStats['CS'] += unranked3['totalMinionKills'];
-                sum2unrankedStats['CS'] += unranked3['totalNeutralMinionsKilled'];
-                sum2unrankedStats['Champion_Kills'] += unranked3['totalChampionKills'];
-                sum2unrankedStats['Assists'] += unranked3['totalAssists'];
-                sum2unrankedStats['Turret_Kills'] += unranked3['totalTurretsKilled'];
-                sum2unrankedStats['Wins'] += entry['wins'];
+                summonerstats['CS'] += unranked3['totalMinionKills'];
+                summonerstats['CS'] += unranked3['totalNeutralMinionsKilled'];
+                summonerstats['Champion_Kills'] += unranked3['totalChampionKills'];
+                summonerstats['Assists'] += unranked3['totalAssists'];
+                summonerstats['Turret_Kills'] += unranked3['totalTurretsKilled'];
+                summonerstats['Wins'] += entry['wins'];
                 break;
             case 'CAP5x5':
                 var teambuilder = entry['aggregatedStats'];
-                sum2unrankedStats['CS'] += teambuilder['totalMinionKills'];
-                sum2unrankedStats['CS'] += teambuilder['totalNeutralMinionsKilled'];
-                sum2unrankedStats['Champion_Kills'] += teambuilder['totalChampionKills'];
-                sum2unrankedStats['Assists'] += teambuilder['totalAssists'];
-                sum2unrankedStats['Turret_Kills'] += teambuilder['totalTurretsKilled'];
-                sum2unrankedStats['Wins'] += entry['wins'];
+                summonerstats['CS'] += teambuilder['totalMinionKills'];
+                summonerstats['CS'] += teambuilder['totalNeutralMinionsKilled'];
+                summonerstats['Champion_Kills'] += teambuilder['totalChampionKills'];
+                summonerstats['Assists'] += teambuilder['totalAssists'];
+                summonerstats['Turret_Kills'] += teambuilder['totalTurretsKilled'];
+                summonerstats['Wins'] += entry['wins'];
                 break;
             case 'AramUnranked5x5':
                 var aram = entry['aggregatedStats'];
                 break;
             case 'Unranked':
                 var unranked = entry['aggregatedStats'];
-                sum2unrankedStats['CS'] += unranked['totalMinionKills'];
-                sum2unrankedStats['CS'] += unranked['totalNeutralMinionsKilled'];
-                sum2unrankedStats['Champion_Kills'] += unranked['totalChampionKills'];
-                sum2unrankedStats['Assists'] += unranked['totalAssists'];
-                sum2unrankedStats['Turret_Kills'] += unranked['totalTurretsKilled'];
-                sum2unrankedStats['Wins'] += entry['wins'];
+                summonerstats['CS'] += unranked['totalMinionKills'];
+                summonerstats['CS'] += unranked['totalNeutralMinionsKilled'];
+                summonerstats['Champion_Kills'] += unranked['totalChampionKills'];
+                summonerstats['Assists'] += unranked['totalAssists'];
+                summonerstats['Turret_Kills'] += unranked['totalTurretsKilled'];
+                summonerstats['Wins'] += entry['wins'];
                 break;
             case 'RankedSolo5x5':
                 var rankedsolo = entry['aggregatedStats'];
                 break;
-            default:
-                
+            default:   
                 break;
         }
     });
-/* Summoner 2 END */ 
+    return summonerstats;
+}
+
+function calculateStats(sum1stats, sum2stats){
+    var stats = {'Unranked': null }
     
-    $('#loader').hide();    
-    $('.stats_table').show();
+    var sum1unrankedStats = getSummonerStats(sum1stats);
+    var sum2unrankedStats = getSummonerStats(sum2stats); 
+
     stats['Unranked1'] = sum1unrankedStats;
     stats['Unranked2'] = sum2unrankedStats;
     setStatTable(stats);
+    $('#loader').hide();    
+    $('.stats_table').show();
 }
 
 function setStatTable(stats){
     var unranked1 = stats['Unranked1'];
     var unranked2 = stats['Unranked2'];
 
-            $('.stats_table thead #sum1').text(stats['sum1Name']);
-            $('.stats_table tbody #CS .sum1').text(unranked1['CS']);
-            $('.stats_table tbody #Champion_Kills .sum1').text(unranked1['Champion_Kills']);
-            $('.stats_table tbody #Assists .sum1').text(unranked1['Assists']);
-            $('.stats_table tbody #Turret_Kills .sum1').text(unranked1['Turret_Kills']);
-            $('.stats_table tbody #Wins .sum1').text(unranked1['Wins']);
+    $('.stats_table thead #sum1').text(window['name1']);
+    $('.stats_table tbody #CS .sum1').text(unranked1['CS']);
+    $('.stats_table tbody #Champion_Kills .sum1').text(unranked1['Champion_Kills']);
+    $('.stats_table tbody #Assists .sum1').text(unranked1['Assists']);
+    $('.stats_table tbody #Turret_Kills .sum1').text(unranked1['Turret_Kills']);
+    $('.stats_table tbody #Wins .sum1').text(unranked1['Wins']);
 
-            $('.stats_table thead #sum2').text(stats['sum2Name']);
-            $('.stats_table tbody #CS .sum2').text(unranked2['CS']);
-            $('.stats_table tbody #Champion_Kills .sum2').text(unranked2['Champion_Kills']);
-            $('.stats_table tbody #Assists .sum2').text(unranked2['Assists']);
-            $('.stats_table tbody #Turret_Kills .sum2').text(unranked2['Turret_Kills']);
-            $('.stats_table tbody #Wins .sum2').text(unranked2['Wins']);
+    $('.stats_table thead #sum2').text(window['name2']);
+    $('.stats_table tbody #CS .sum2').text(unranked2['CS']);
+    $('.stats_table tbody #Champion_Kills .sum2').text(unranked2['Champion_Kills']);
+    $('.stats_table tbody #Assists .sum2').text(unranked2['Assists']);
+    $('.stats_table tbody #Turret_Kills .sum2').text(unranked2['Turret_Kills']);
+    $('.stats_table tbody #Wins .sum2').text(unranked2['Wins']);
 
     }
 
@@ -316,7 +235,14 @@ function succes_message(){
     $( "#success_popup" ).popup( "open");
 }
 // End Popups
-
+function resetScore(){
+    $('.stats_table td').removeClass('winner');
+    $('.stats_table td').removeClass('loser');
+    $('.stats_table thead th').removeClass('winner');
+    $('.stats_table thead th').removeClass('loser');
+    $('.stats_table tr').removeClass('selected');
+    $('.stats_table thead #score').text('Score: 0 - 0');
+}
 function redrawScore(){
     var score = $('.stats_table thead #score');
     var sum1score = $('td.sum1.winner');
@@ -342,6 +268,7 @@ function redrawScore(){
 }
 
 $('tbody tr').on('touchstart', function(){
+    console.log('test');
     var row = $(this);
     var sum1cell = row.find('td.sum1');
     var sum2cell = row.find('td.sum2');
@@ -356,5 +283,4 @@ $('tbody tr').on('touchstart', function(){
     }
     row.toggleClass('selected');
     redrawScore();
-
 });
