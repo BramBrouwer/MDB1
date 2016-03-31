35b293e426f93352132e43f52ce98edbbee65c15
bbrouwer <b.brouwer@student.avans.nl>
2016-03-31 21:15:17 +0200

    added body, removed test lines

--- www/js/contacts.js
+++ www/js/contacts.js
@@ -3,14 +3,9 @@
 //remove loader and show list on contacts loaded
 $("#loadcontacts").click(function() {
                
-                // console.log("contacts called build pls");
-                 var name = "naam";
-                 var phone= 66666666;
-                // Add item
+               
                 // $("#contactslist").append('<li><a href="sms://+31612345678?body=1V1%20me%20bruh%20www.downloadlinknaaronzeapp.com/">AAAAATEST</a></li>');
-                 $("#contactslist").append('<li><a href="sms://'+phone+'">'+name+'</a></li>');
-                // Refresh list to update styling
-                 $( "#contactslist" ).listview( "refresh" );
+              
                 
                 var fields = ["displayName", "name","phoneNumbers"];
                 navigator.contacts.find(fields, onSuccess, onError);
@@ -32,17 +27,13 @@ function onSuccess(contacts)
         {
             
                 if(contacts[i].displayName != null && contacts[i].phoneNumbers[0] != null){
-                    var name = contacts[i].displayName;
-                    var phone = contacts[i].phoneNumbers[0];
-                        //  $("#contactslist").append('<li> displayname: '+contacts[i].displayName+'</li>');
-                        //    $("#contactslist").append('<li><a href="sms://'+contacts[i].phoneNumbers[0].value+'">'+contacts[i].displayName+'</a></li>');
-                        $("#contactslist").append('<li><a href="sms://'+phone+'">'+name+'</a></li>');
-                                                $("#contactslist").append('<li><a href="sms://'+"test"+'">'+"test"+'</a></li>');
-                                                
-                                                  $( "#contactslist" ).listview( "refresh" );
+                        var name = contacts[i].displayName;
+                        var phone = contacts[i].phoneNumbers[0];
+                        $("#contactslist").append('<li><a href="sms://'+phone+'?body=1V1%20me%20bruh%20www.downloadlinknaaronzeapp.com/">'+name+'</a></li>');
+                        $( "#contactslist" ).listview( "refresh" );
          }
       }
-            $( "#contactslist" ).listview( "refresh" );
+
         
     }
 
