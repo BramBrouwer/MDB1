3dfb5df4884d48d0dbfe0ec75089a6540aec353c
bbrouwer <b.brouwer@student.avans.nl>
2016-03-31 13:13:13 +0200

    werkt het?

--- www/js/popups.js
+++ www/js/popups.js
@@ -1,31 +1,31 @@
 
- // Wait for device API libraries to load
-            document.addEventListener("deviceready", onDeviceReady, false);
+//  // Wait for device API libraries to load
+//             document.addEventListener("deviceready", onDeviceReady, false);
 
-            // device APIs are available
+//             // device APIs are available
 
-            function onDeviceReady() {
-                // find all contacts with 'Bob' in any name field
-                console.log('device ready ofzo');
-                var options = new ContactFindOptions();
-                options.filter = "Alec";
-                var fields = ["displayName", "name"];
-                navigator.contacts.find(fields, onSuccess, onError, options);
-            }
+//             function onDeviceReady() {
+//                 // find all contacts with 'Bob' in any name field
+//                 console.log('device ready ofzo');
+//                 var options = new ContactFindOptions();
+//                 options.filter = "Alec";
+//                 var fields = ["displayName", "name"];
+//                 navigator.contacts.find(fields, onSuccess, onError, options);
+//             }
 
-            // onSuccess: Get a snapshot of the current contacts
+//             // onSuccess: Get a snapshot of the current contacts
 
-            function onSuccess(contacts) {
-                for (var i = 0; i < contacts.length; i++) {
-                    alert("Display Name = " + contacts[i].displayName);
-                }
-            }
+//             function onSuccess(contacts) {
+//                 for (var i = 0; i < contacts.length; i++) {
+//                     alert("Display Name = " + contacts[i].displayName);
+//                 }
+//             }
 
-            // onError: Failed to get the contacts
+//             // onError: Failed to get the contacts
 
-            function onError(contactError) {
-                alert('onError!');
-            }
+//             function onError(contactError) {
+//                 alert('onError!');
+//             }
 
 
 
