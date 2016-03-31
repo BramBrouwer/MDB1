4948b4a0213932ab997eb13b062a2221a81dd6d6
bbrouwer <b.brouwer@student.avans.nl>
2016-03-31 15:08:25 +0200

    adsadsadads

--- www/js/contacts.js
+++ www/js/contacts.js
@@ -2,6 +2,12 @@
 $("#loadcontacts").click(function() {
                
                 console.log("contacts called");
+                
+                // Add item
+                $("#contactslist").append('<li><a href="sms://+31612345678?body=1V1%20me%20bruh%20www.downloadlinknaaronzeapp.com/">AAAAATEST</a></li>');
+                // Refresh list to update styling
+                 $( "#contactslist" ).listview( "refresh" );
+                
                 var fields = ["displayName", "name"];
                 navigator.contacts.find(fields, onSuccess, onError);
                 
@@ -18,8 +24,10 @@ $("#loadcontacts").click(function() {
             function onSuccess(contacts) {
                 alert("Contacts found: " + contacts.length)
                  for (var i = 0; i < contacts.length; i++) {
-                     $("contactslist").append($("<li>".text(contacts[i].displayName)));
+                    //  $("contactslist").append($("<li>".text(contacts[i].displayName)));
+                     $("#contactslist").append('<li><a href="sms://+31612345678?body=1V1%20me%20bruh%20www.downloadlinknaaronzeapp.com/">'+contacts[i].displayName+'</a></li>');
                  }
+                                 $( "#contactslist" ).listview( "refresh" );
                     
                    
 
