90e92baf1101cf45c259a51c21c1964529f53a51
bbrouwer <b.brouwer@student.avans.nl>
2016-04-01 01:56:46 +0200

    Perform contact retrievel on page init, slide transition added

--- www/js/contacts.js
+++ www/js/contacts.js
@@ -1,10 +1,14 @@
-$("#loadcontacts").tap(function() {
-    $("#loadcontacts").hide();
+$( document ).on( "pageinit", "#contactspage", function( event ) {
     $( "#contactslist" ).hide();
     $( "#contactsloader" ).show();
     var fields = ["displayName", "name","phoneNumbers"];
-    navigator.contacts.find(fields, onSuccess, onError);            
+    navigator.contacts.find(fields, onSuccess, onError); 
 });
+
+
+
+
+
 
 
 function onSuccess(contacts) 
