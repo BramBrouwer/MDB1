772d767df7689d476398f1aa731f759efbd5305b
bbrouwer <b.brouwer@student.avans.nl>
2016-03-31 13:07:33 +0200

    pls werk

--- www/js/contacts.js
+++ www/js/contacts.js
@@ -1,23 +1,27 @@
-// A $( document ).ready() block.
-$( document ).ready(function() {
-   
-   
-     var options = new ContactFindOptions();
-        options.filter = "Alec";
-        var fields = ["displayName", "name"];
-                navigator.contacts.find(fields, onSuccess, onError, options);
-            
 
-           
-   
+$("#contactstest").click(function() {
+                alert("knop");
+                //  var options = new ContactFindOptions();
+                // options.filter = "Bob";
+                var fields = ["displayName", "name"];
+                navigator.contacts.find(fields, onSuccess, onError);
+                
+
 });
+
+   
+   
+   
+ 
 
  // onSuccess: Get a snapshot of the current contacts
 
             function onSuccess(contacts) {
-                for (var i = 0; i < contacts.length; i++) {
-                    alert("Display Name = " + contacts[i].displayName);
-                }
+                // for (var i = 0; i < contacts.length; i++) {
+                //     alert("Display Name = " + contacts[i].displayName);
+                // }
+                    alert('Found ' + contacts.length + ' contacts.');
+
             }
 
             // onError: Failed to get the contacts
--- www/js/popups.js
+++ www/js/popups.js
@@ -1,4 +1,41 @@
 
+ // Wait for device API libraries to load
+            document.addEventListener("deviceready", onDeviceReady, false);
+
+            // device APIs are available
+
+            function onDeviceReady() {
+                // find all contacts with 'Bob' in any name field
+                console.log('device ready ofzo');
+                var options = new ContactFindOptions();
+                options.filter = "Alec";
+                var fields = ["displayName", "name"];
+                navigator.contacts.find(fields, onSuccess, onError, options);
+            }
+
+            // onSuccess: Get a snapshot of the current contacts
+
+            function onSuccess(contacts) {
+                for (var i = 0; i < contacts.length; i++) {
+                    alert("Display Name = " + contacts[i].displayName);
+                }
+            }
+
+            // onError: Failed to get the contacts
+
+            function onError(contactError) {
+                alert('onError!');
+            }
+
+
+
+
+
+
+
+
+
+
 var sites = 
 ["http://ducksarethebest.com/",
 "http://www.staggeringbeauty.com/",
