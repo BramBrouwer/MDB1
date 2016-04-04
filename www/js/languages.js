288c20388fd0a717c9717bdcb4238dc387987870
bbrouwer <b.brouwer@student.avans.nl>
2016-04-04 19:56:39 +0200

    added language support for settings page

--- www/js/languages.js
+++ www/js/languages.js
@@ -1,4 +1,19 @@
 
+$(document).on("pagebeforeshow","#settingspage", function(){
+  console.log("setttingspage before show called");
+  var a = window.localStorage.getItem("pref_lang");
+   
+   switch(a) {
+    case null:
+        settings_eng();
+        break;
+    case "eng":
+        settings_eng();
+        break;
+    case "nl":
+        settings_nl();
+   }
+});
 
 $(document).on("pagebeforeshow","#homepage", function(){
   console.log("homepage before show called");
@@ -95,3 +110,50 @@ function contacts_nl(){
    $("#back_button").text(nl_strings.back_button);
    $("#loadcontacts").text(nl_strings.contacts_button);
 }
+
+function settings_eng(){
+    var eng_strings = 
+  {
+    header: "Settings",
+    back_button:  "back",
+    settings_theme: "Theme",
+    theme_day: "Day",
+    theme_night: "Night",
+    settings_region: "Region",
+    settings_language: "Language",
+    settings_submit: "Save"
+  }
+    
+    $("#settings_header").text(eng_strings.header);
+    $("#settings_back_button").text(eng_strings.back_button);
+    $("#settings_theme").text(eng_strings.settings_theme);
+    $("label[for='day']").text(eng_strings.theme_day);
+    $("label[for='night']").text(eng_strings.theme_night);
+    $("#settings_region").text(eng_strings.settings_region);
+    $("#settings_language").text(eng_strings.settings_language);
+    $("#settings_submit").text(eng_strings.settings_submit);
+  };
+  
+  function settings_nl(){
+       var nl_strings = 
+  {
+    header: "Instellingen",
+    back_button:  "terug",
+    settings_theme: "Thema",
+    theme_day: "Dag",
+    theme_night: "Nacht",
+    settings_region: "Regio",
+    settings_language: "Taal",
+    settings_submit: "Opslaan"
+  }
+  
+    $("#settings_header").text(nl_strings.header);
+    $("#settings_back_button").text(nl_strings.back_button);
+    $("#settings_theme").text(nl_strings.settings_theme);
+    $("label[for='day']").text(nl_strings.theme_day);
+    $("label[for='night']").text(nl_strings.theme_night);
+    $("#settings_region").text(nl_strings.settings_region);
+    $("#settings_language").text(nl_strings.settings_language);
+    $("#settings_submit").text(nl_strings.settings_submit);
+    
+  }
