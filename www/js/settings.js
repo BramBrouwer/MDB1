35439636e410b6c44db65b240e822621438338ac
Alec <alec.blom@hotmail.com>
2016-04-04 14:04:45 +0200

    Settings is klaar

--- www/js/settings.js
+++ www/js/settings.js
@@ -2,40 +2,37 @@ $(document).on("pagebeforeshow","#settingspage", function(){
     var color = window.localStorage.getItem("pref_color");
     var region = window.localStorage.getItem("pref_region");
     var lang = window.localStorage.getItem("pref_lang");
-    var selected, label;
-console.log(region);
+    
     switch(color){
         case 'day':
-            $('input[id=day]').attr('checked', true);
+            $('input[id=day]').attr('checked', true).checkboxradio('refresh');
             break;
         case 'night':
-            $('input[id=night]').attr('checked', true);
+            $('input[id=night]').attr('checked', true).checkboxradio('refresh');
             break;
         default:
-            $('input[id=night]').attr('checked', true);
+            $('input[id=night]').attr('checked', true).checkboxradio('refresh');
             break;
     }
     switch(region){
         case null:
+            $("select#regionlist").val('euw').selectmenu("refresh");
             $('input[value=euw]').attr('selected', 'selected');
             break;
         default:
-            label = $('#regionlist-button span');
-            selected = $('option[value=' + region + ']');
-            selected.attr('selected', 'selected');
-            console.log(selected.text());
-            label.text(selected.text());
+            $("select#regionlist").val(region).selectmenu("refresh");
+            $('input[value='+ region +']').attr('selected', 'selected');
             break;
     }
     switch(lang){
         case 'eng':
-            $('input[id=eng]').attr('checked', true);
+            $('input[id=eng]').attr('checked', true).checkboxradio('refresh');
             break;
         case 'nl':
-            $('input[id=nl]').attr('checked', true);
+            $('input[id=nl]').attr('checked', true).checkboxradio('refresh');
             break;
         default:
-            $('input[id=eng]').attr('checked', true);
+            $('input[id=eng]').attr('checked', true).checkboxradio('refresh');
             break;
     }
 
@@ -45,8 +42,9 @@ console.log(region);
         var color =  form.find('input[name=colors]:checked').attr('id');
         var region = form.find('option[name=region]:selected').attr('value');
         var lang =  form.find('input[name=lang]:checked').attr('id');
-        console.log('test');
+        
         window.localStorage.setItem("pref_color", color);
         window.localStorage.setItem("pref_region", region);
         window.localStorage.setItem("pref_lang", lang);
+        $('#regionlist').selectmenu('refresh');
     }
