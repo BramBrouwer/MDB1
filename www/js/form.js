279c309308c90befc06b3f82ed38972ad858f0b1
Alec <alec@Alec-Laptop>
2016-04-04 17:44:36 +0200

    Revert "id pls"
    This reverts commit acd30e98cc2cb4f303ba41f5c595a9644d4cbbd6.
    
    # Conflicts:
    #	config.xml

--- www/js/form.js
+++ www/js/form.js
@@ -18,15 +18,18 @@ $('#target').submit(function() {
     // Disable button to prevent the user breaking stuff
     $('input[type="submit"]').prop('disabled', true);
  
-    //validate names and retrieve stats if names are valid
-    verifySummoners(name1,name2);
-    
-    if(name1 == "" || name2 == "")
+     if(name1 == "" || name2 == "")
     {
+        $('#loader').hide();
         empty_name_error();
         $('input[type="submit"]').prop('disabled', false);
         return false;
     }
+ 
+    //validate names and retrieve stats if names are valid
+    verifySummoners(name1,name2);
+    
+   
     return false;
 });
 
